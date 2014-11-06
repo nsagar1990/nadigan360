aacd90b403e4c613f8aa532243dff40b86717e08
root <nsagar1990@yahoo.co.in>
2014-11-06 00:38:13 -0500

    changing template dirs

--- nadigan360/nadigan360/settings.py
+++ nadigan360/nadigan360/settings.py
@@ -112,7 +112,7 @@ ROOT_URLCONF = 'nadigan360.urls'
 WSGI_APPLICATION = 'nadigan360.wsgi.application'
 
 TEMPLATE_DIRS = (
-    '/home/niranjan/django_projects/nadigan360/templates'
+    '/home/niranjan/django_projects/nadigan360/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
