46c71d226e7278bf77f9e3d20ce039d4a8636af5
nsagar1990 <nsagar1990@yahoo.co.in>
2015-01-13 04:59:54 -0500

    simple commit

--- nadigan360/nadigan360/urls.py
+++ nadigan360/nadigan360/urls.py
@@ -11,10 +11,9 @@ urlpatterns = patterns('nd360.views',
     	#url(r'^nadigan360/', include('nadigan360.foo.urls')),
 	url(r'home_data/$', 'home_data', name="home_data"),
 	url(r'gallery/(?P<gallery_id>\d+)/$', 'gallery', name="gallery"),
-
-    	# Uncomment the admin/doc line below to enable admin documentation:
-    	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'reviews/(?P<review_id>\d+)/$', 'reviews', name="reviews"),		
 
    	# Uncomment the next line to enable the admin:
     	url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
