2b8b0efaa0eb770dbb6336631359adfe8854f816
nsagar1990 <nsagar1990@yahoo.co.in>
2015-01-13 04:45:34 -0500

    removed unwanted files

--- nadigan360/django_wsgi.py
+++
@@ -1,7 +0,0 @@
-import os, sys 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'nadigan360.settings'
-
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
